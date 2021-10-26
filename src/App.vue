61fd1ede52ddd43dabe93f1ea7e1f135db518ff2
Julia Malay <malayjulia14@gmail.com>
2021-10-26 23:10:38 +0700

    created component Task

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form @submit.prevent>
+    <input
+      v-bind:value="title"
+      @input="title = $event.target.value"
+      type="text"
+      name=""
+      id=""
+      placeholder=""
+    />
+    <button @click="createTask">Создать</button>
+    <input type="checkbox" v-model="important" />
+  </form>
+  <div class="task" :key="task.id" v-for="task in tasks">
+    <input
+      v-model="task.checked"
+      v-bind:id="task.id"
+      type="checkbox"
+      name="check"
+    />
+    <div>
+      <strong>{{ task.title }}</strong>
+    </div>
+    <div>{{ task.date }}</div>
+    <div v-show="task.important" class="important"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Task from './components/Task.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'First Task',
+          date: '14:10',
+          checked: false,
+          important: false,
+        },
+        {
+          id: 2,
+          title: 'Second Task',
+          date: '16:40',
+          checked: false,
+          important: false,
+        },
+        {
+          id: 3,
+          title: 'Third Task',
+          date: '21.11.2021, 12:44:57',
+          checked: false,
+          important: false,
+        },
+      ],
+      title: '',
+      important: false,
+    };
+  },
+  methods: {
+    createTask() {
+      const newTask = {
+        id: Date.now(),
+        title: this.title,
+        date: new Date().toLocaleString('ru-RU'),
+        checked: false,
+        important: this.important,
+      };
+      this.tasks.push(newTask);
+      this.important = false;
+      this.title = '';
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +86,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.task {
+  display: flex;
+  align-items: center;
+}
+.important {
+  background-color: red;
+  border-radius: 50%;
+  width: 10px;
+  height: 10px;
 }
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="task">
+    <h3>{{ title }}</h3>
+    <p>{{ date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'task',
+  props: {
+    title: String,
+    date: String,
+  },
+};
+</script>
+
+<style scoped></style>
